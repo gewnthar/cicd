e866e834a5fe9741f04ab6191c85157d89b2edb0
Gewnthar <vienebulagaming@gmail.com>
2025-11-20 22:56:33 -0500

    fix: resolve gosec security warnings round 2

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
@@ -91,8 +90,8 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: time.Minute,
 	}
 
